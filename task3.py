f870770e5100eb65e5c9414514496c2762d2dd5e
Jmiller7023 <fireyshyguy@gmail.com>
2019-09-26 18:24:06 -0700

    push python file for part 3

---
+++ task3.py
@@ -0,0 +1,8 @@
+if __name__ == '__main__':
+    x = "EXE_180_DA_DB"
+    if x == "EXE_180_DA_DB":
+        print("You are living in 2017")
+    else:
+        # this is a comment
+        x = x + " - Best class ever"
+        print(x)
